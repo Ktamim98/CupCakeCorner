8ace22f9ace872da0151ea527747298f9881737e
Ktamim98 <tamim15-2515@diu.edu.bd>
2023-04-27 00:31:24 +0600

    .updated

--- CupCakeCorner/CheckoutView.swift
+++ CupCakeCorner/CheckoutView.swift
@@ -10,10 +10,73 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confarmationMessage = ""
+    @State private var showingConfarmation = false
+    @State private var showAlert = false
+    
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack{
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3){ image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                }placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("your total cost is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                Button("place order"){
+                    Task{
+                        await placeOrder()
+                    }
+                }
+                    .padding()
+            }
+        }
+        .navigationTitle("Check Out")
+        .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!" ,isPresented: $showingConfarmation){
+            Button("ok"){}
+        }message: {
+            Text(confarmationMessage)
+        }
+        
+        .alert("Oops" ,isPresented: $showAlert){
+            Button("ok"){}
+        }message: {
+            Text("no internet")
+        }
     }
+    
+    func placeOrder() async{
+        guard let encode = try? JSONEncoder().encode(order) else{
+            print("faild to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do{
+            let(data, _) = try await URLSession.shared.upload(for: request, from: encode)
+            
+            let decodeOrder = try JSONDecoder().decode(Order.self, from: data)
+            confarmationMessage = "your order for \(decodeOrder.quantity)x \(Order.types[decodeOrder.type].lowercased()) is on the way"
+            showingConfarmation = true
+            
+        }catch{
+            showAlert = true
+            print("check out faild")
+        }
+        
+    }
+    
 }
 
 struct CheckoutView_Previews: PreviewProvider {
